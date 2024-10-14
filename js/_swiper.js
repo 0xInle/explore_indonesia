d62857a6c2b05640065fe73957fa0445ebcf3d66
0xInle <xsk8er26x@gmail.com>
2024-10-14 23:38:20 +0300

    Реализация свайпера блока header

---
+++ js/_swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  parallax: true,
+  speed: 1000,
+  keyboard: {
+    enabled: true
+  },
+
+
+  pagination: {
+    el: '.header__counter-count',
+    type: 'fraction'
+  },
+
+  navigation: {
+    nextEl: '.headerNext',
+    prevEl: '.headerPrev',
+  },
+
+  scrollbar: {
+    el: '.header__scrollbar',
+  },
+});
